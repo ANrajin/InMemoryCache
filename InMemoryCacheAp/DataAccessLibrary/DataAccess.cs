3d4f6177ceb8a15984f049f18ee58cdbf4cce27e
ANrajin <an.rajin@gmail.com>
2021-12-24 22:51:49 +0600

    initial commit of this repo

---
+++ InMemoryCacheAp/DataAccessLibrary/DataAccess.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class DataAccess
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public DataAccess(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+
+        }
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> employees = new();
+
+            employees.Add(new EmployeeModel { FirstName = "Jhone", LastName = "Doe" });
+            employees.Add(new EmployeeModel { FirstName = "Robert", LastName = "Martin" });
+            employees.Add(new EmployeeModel { FirstName = "Angela", LastName = "Martin" });
+            employees.Append(new EmployeeModel { FirstName = "Sean", LastName = "Medas" });
+
+            Thread.Sleep(3000);
+
+            return employees;
+        }
+
+
+        public async Task<List<EmployeeModel>> GetEmployeesAsync()
+        {
+            List<EmployeeModel> employees = new();
+
+            employees.Add(new EmployeeModel { FirstName = "Jhone", LastName = "Doe" });
+            employees.Add(new EmployeeModel { FirstName = "Robert", LastName = "Martin" });
+            employees.Add(new EmployeeModel { FirstName = "Angela", LastName = "Martin" });
+            employees.Append(new EmployeeModel { FirstName = "Sean", LastName = "Medas" });
+
+            await Task.Delay(3000);
+
+            return employees;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesCache()
+        {
+            List<EmployeeModel> employees;
+
+            //get the data if it is exist in cache
+            employees = _memoryCache.Get<List<EmployeeModel>>("employees");
+
+            //cache the data if not exist
+            if(employees is null)
+            {
+                employees = new();
+
+                employees.Add(new EmployeeModel { FirstName = "Jhone", LastName = "Doe" });
+                employees.Add(new EmployeeModel { FirstName = "Robert", LastName = "Martin" });
+                employees.Add(new EmployeeModel { FirstName = "Angela", LastName = "Martin" });
+                employees.Append(new EmployeeModel { FirstName = "Sean", LastName = "Medas" });
+
+                await Task.Delay(3000);
+
+                //cache into memory
+                _memoryCache.Set("employees", employees, TimeSpan.FromMinutes(1));
+            }
+
+            return employees;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # InMemoryCache
-This is practice app of Asp.NET
+
+This app is a simple work of using Microsoft build in In memory cache library.
+Caching data help us to load the data form cache, so that the user have a
+better experience. Although there is a better way of caching with Redis.
+We can use this InMemory cache library form small app.
+
+Nuget Package: Microsoft.Extensions.Caching.Memory
+
+For more: https://docs.microsoft.com/en-us/aspnet/core/performance/caching/memory?view=aspnetcore-6.0#cache-dependencies
